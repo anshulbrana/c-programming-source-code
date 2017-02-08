74e29f45e4da7fbc192db8f75f9292c0ec2c1b88
Rupak Rana <Rupak Rana>
2017-02-08 14:59:03 +0545

    first

---
+++ swap using pass by refrence.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<conio.h>
+using namespace std;
+void swap(int &n1,int &n2){
+	int temp;
+	temp=n1;
+	n1=n2;
+	n2=temp;
+}
+int main(){
+	int n1,n2;
+	cout<<"Enter the value of two numbers"<<endl;
+	cin>>n1>>n2;
+	swap(n1,n2);
+	cout<<"The numbers after swap is"<<endl<<n1<<endl<<n2;
+	getch();
+	return 0;
+}
